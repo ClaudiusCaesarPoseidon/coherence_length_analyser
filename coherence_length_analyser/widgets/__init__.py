292342f71e7eca3a1986628bcd16f239e70b21a4
ClaudiusCaesarPoseidon <47826618+ClaudiusCaesarPoseidon@users.noreply.github.com>
2019-02-20 17:57:05 +0100

    Delete __init__.py

--- coherence_length_analyser/widgets/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import canvas
-from . import combobox
-from . import label
-from . import lineedit
-from . import plaintextedit
-from . import pushbutton
-from . import pyinterp
-from . import toolbutton
-name = "widgets"
