37a0dd32f8edf51172b3552f549920fb818ce010
ClaudiusCaesarPoseidon <47826618+ClaudiusCaesarPoseidon@users.noreply.github.com>
2019-02-20 18:10:43 +0100

    Delete __init__.py

--- coherence_length_analyser/modules/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import analyser
-from . import camera
-from . import eigen_widgets
-from . import register
-from . import check_fft
-from . import angle
-from . import turn
-from . import master
-name = "modules"
