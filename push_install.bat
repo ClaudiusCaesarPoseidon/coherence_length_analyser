94408bab042e0a61c7475f9b7df30699ea7a2c32
claudiuscaesarposeidon <haarmeyer@out-ev.de>
2019-02-21 14:57:53 +0100

    glubber

--- push_install.bat
+++ push_install.bat
@@ -1,3 +1,5 @@
-push.bat
-install.bat
+git add .
+git commit -m "glubber"
+git push origin master
+pip install git+https://github.com/ClaudiusCaesarPoseidon/coherence_length_analyser --user --force-reinstall --no-deps
 pause
