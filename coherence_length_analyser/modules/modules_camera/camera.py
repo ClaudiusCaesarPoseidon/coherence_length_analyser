4916fb22b707de909b28b302c95a51ebbec74099
claudiuscaesarposeidon <haarmeyer@out-ev.de>
2019-02-22 13:24:27 +0100

    glubber

--- coherence_length_analyser/modules/modules_camera/camera.py
+++ coherence_length_analyser/modules/modules_camera/camera.py
@@ -126,7 +126,6 @@ class Camera(Widgetb):
     def set_gui_values(self):
         temp = self.Values_GUI.toPlainText().split("\n")
         tmp = [y.split("\t") for y in temp]
-#        print(tmp)
         tmp[0][5] = str(self.angle)
         tmp[1][6] = str(self.lines)
         tmp[1][1] = str(self.exposure_current)
@@ -149,6 +148,7 @@ class Camera(Widgetb):
         tmp = ''.join(tmp)
         self.Info.setPlainText(tmp)
 
+    @QtCore.Slot(tuple)
     def do_connect(self, tup):
         ser, came = tup
         self.ser = ser
@@ -177,6 +177,7 @@ class Camera(Widgetb):
         self.thread.val.connect(self.lock)
         self.thread.White.connect(self.set_white)
 
+    @QtCore.Slot()
     def do_not_connect(self, tup):
         self.Cam_Active = False
         ser, came = tup
