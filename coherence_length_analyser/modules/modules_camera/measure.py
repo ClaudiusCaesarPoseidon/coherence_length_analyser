5931a78ba65a4af6b3efdcd991c49a3118f7267b
claudiuscaesarposeidon <haarmeyer@out-ev.de>
2019-02-26 15:32:10 +0100

    glubber

--- coherence_length_analyser/modules/modules_camera/measure.py
+++ coherence_length_analyser/modules/modules_camera/measure.py
@@ -59,7 +59,7 @@ class Measure(QtCore.QThread):
             pass
         else:
             ImageData = functions.np.zeros(
-                (480, 640), dtype=functions.np.uint8)
+                (480, 480), dtype=functions.np.uint8)
             exposure = 0.0
             gain = 50
             if self.parent.Lock.isChecked() is False:
@@ -92,9 +92,9 @@ class Measure(QtCore.QThread):
                     exposure, gain = functions.Set_Values(
                         self.parent.cam, exposure, gain, 114, False)
                     functions.CopyImg(self.parent.cam, ImageData,self.parent.pcImgMem, self.parent.pid)
-                    self.img = ImageData.copy()
-                    self.img = np.roll(self.img, -40, axis=1)
-                    self.img = np.delete(self.img, np.s_[480:640], axis=1)
+#                    self.img = ImageData.copy()
+#                    self.img = np.roll(self.img, -40, axis=1)
+#                    self.img = np.delete(self.img, np.s_[480:640], axis=1)
                     dft = functions.dft(self.img)
                     fft = functions.fft_cv2(dft)
                     fft = functions.fft_shift_py(
