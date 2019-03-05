3035b583a52bd25869cc42d5d26b2c2aa46d5d1d
claudiuscaesarposeidon <haarmeyer@out-ev.de>
2019-03-05 14:31:41 +0100

    glubber

--- coherence_length_analyser/modules/modules_camera/measure.py
+++ coherence_length_analyser/modules/modules_camera/measure.py
@@ -48,130 +48,128 @@ class Measure(QtCore.QThread):
 
     def run(self):
         # create unique directory
-        self.string_direc = datetime.datetime.now().strftime("%Y-%m-%d_%H.%M.%S") + "_" + self.ID.text() + \
+        self.direc_name = datetime.datetime.now().strftime("%Y-%m-%d_%H.%M.%S") + "_" + self.ID.text() + \
             "_" + self.Max_Width.text() + '_' + self.Temperature.text() + \
             "_" + self.Current.text()
-        self.string_direc2 = os.path.join(
-            self.parent.direc_path, self.string_direc)
-        print(self.string_direc)
-        print()
-        print(self.string_direc2)
-#        os.makedirs(self.string_direc2, exist_ok=True)
+        self.string_direc = os.path.join(
+            self.parent.direc_path, self.direc_name)
+        os.makedirs(self.string_direc, exist_ok=True)
 #        ret1 = functions.build_directory(self.string_direc)
 #        functions.remove_directory(self.string_direc)
-#
-#        self.parent.thread_run = True
-#        if self.parent.failed is True:
-#            ret1 = 1
-#        if ret1 == 1:
-#            pass
-#        else:
-#            ImageData = functions.np.zeros(
-#                (480, 480), dtype=functions.np.uint8)
-#            exposure = 0.0
-#            gain = 50
-#            if self.parent.Lock.isChecked() is False:
-#                exposure, gain = functions.Set_Values(
-#                    self.parent.cam, exposure, gain, 114, True)
-#            else:
-#                if self.parent.exposure_saved is not None and self.parent.gain_saved is not None:
-#                    exposure, gain = self.parent.exposure_saved, self.parent.gain_saved
-#                    exposure, gain = functions.Set_Values(
-#                        self.parent.cam, exposure, gain, 114, False)
-#                else:
-#                    exposure, gain = functions.Set_Values(
-#                        self.parent.cam, exposure, gain, 114, True)
-#            start = True
-#            print('Start')
-#            mode = None
-#            if self.mode == "forward":
-#                mode = 'f'
-#            elif self.mode == "backward":
-#                mode = 'b'
-#            while True:
-#                if self.parent.failed is True:
-#                    break
-#                if self.parent.ret == 0:
-#                    if start is True:
-#                        self.sleep(2)
-#                        start = False
-#                        exposure, gain = functions.Get_Values(
-#                            self.parent.cam, exposure)
-#                    exposure, gain = functions.Set_Values(
-#                        self.parent.cam, exposure, gain, 114, False)
-#                    # copy image from memory to numpy array
-#                    functions.CopyImg(
-#                        self.parent.cam,
-#                        ImageData,
-#                        self.parent.pcImgMem,
-#                        self.parent.pid)
-#                    self.img = ImageData.copy()
-#                    self.img = np.roll(self.img, 15, axis=0)
-#
-#                    # calculate fft
-#                    dft = functions.dft(self.img)
-#                    fft = functions.fft_cv2(dft)
-#                    fft = functions.fft_shift_py(
-#                        fft.astype(np.float64)).astype(np.uint8)
-#
-#                    # display image
-#                    res = np.concatenate((self.img, fft), axis=0)
-#                    convertToQtFormat = qimage2ndarray.gray2qimage(res)
-#                    p = convertToQtFormat.scaled(
-#                        self.parent.bild_width,
-#                        self.parent.bild_height,
-#                        QtCore.Qt.KeepAspectRatio)
-#                    self.changePixmap.emit(p)
-#
-#                    # save imgae in directory
-#                    print("step\t" + str(self.j + 1) + "/" + str(self.max_int)
-#                          + "\t " + str(self.parent.i) + "/" +
-#                          str(self.parent.number_of_measurements))
-#                    if mode is not None:
-#                        tmp1 = "img" + mode
-#                        tmp2 = "_%d_" % self.j + self.Max_Width.text()
-#                        tmp3 = '_' + self.Temperature.text() + "_" + self.Current.text()
-#                        tmp4 = "_" + self.number.text() + ".png"
-#                        tmp5 = tmp1 + tmp2 + tmp3 + tmp4
-#                        cv2.imwrite(os.path.join(
-#                            self.string_direc2, tmp5), self.img)
-#                    else:
-#                        tmp1 = "img"
-#                        tmp2 = "_%d_" % self.j + self.Max_Width.text()
-#                        tmp3 = '_' + self.Temperature.text() + "_" + self.Current.text()
-#                        tmp4 = "_" + self.number.text() + ".png"
-#                        tmp5 = tmp1 + tmp2 + tmp3 + tmp4
-#                        cv2.imwrite(os.path.join(
-#                            self.string_direc2, tmp5), self.img)
-#
-#                    # end if max width is reached
-#                    if self.i < float(self.Max_Width.text()):
-#                        for k in range(int(self.number.text())):
-#                            if self.parent.failed is True:
-#                                break
-#                            if self.mode == "forward":
-#                                tmp = self.parent.position.copy()
-#                                tmp[0][0] += 0.11
-#                                self.parent.position = tmp
-#                            elif self.mode == "backward":
-#                                tmp = self.parent.position.copy()
-#                                tmp[0][0] -= 0.11
-#                                self.parent.position = tmp
-#                            params = (self.parent.ser,)
-#                            getattr(functions, self.mode)(*params)
-#                            functions.save_txt(
-#                                self.parent.pos_path, self.parent.position)
-#                            self.msleep(10)
-#                        self.do.emit()
-#                        self.i += self.step
-#                        self.j += 1
-#                        self.msleep(150)
-#                    else:
-#                        self.ends = True
-#                if self.i >= float(self.Max_Width.text()):
-#                    break
-#                if self.parent.ends is True:
-#                    break
-#                if self.parent.failed is True:
-#                    break
-#        print("Finished")
+        ret1 = 0
+
+        self.parent.thread_run = True
+        if self.parent.failed is True:
+            ret1 = 1
+        if ret1 == 1:
+            pass
+        else:
+            ImageData = functions.np.zeros(
+                (480, 480), dtype=functions.np.uint8)
+            exposure = 0.0
+            gain = 50
+            if self.parent.Lock.isChecked() is False:
+                exposure, gain = functions.Set_Values(
+                    self.parent.cam, exposure, gain, 114, True)
+            else:
+                if self.parent.exposure_saved is not None and self.parent.gain_saved is not None:
+                    exposure, gain = self.parent.exposure_saved, self.parent.gain_saved
+                    exposure, gain = functions.Set_Values(
+                        self.parent.cam, exposure, gain, 114, False)
+                else:
+                    exposure, gain = functions.Set_Values(
+                        self.parent.cam, exposure, gain, 114, True)
+            start = True
+            print('Start')
+            mode = None
+            if self.mode == "forward":
+                mode = 'f'
+            elif self.mode == "backward":
+                mode = 'b'
+            while True:
+                if self.parent.failed is True:
+                    break
+                if self.parent.ret == 0:
+                    if start is True:
+                        self.sleep(2)
+                        start = False
+                        exposure, gain = functions.Get_Values(
+                            self.parent.cam, exposure)
+                    exposure, gain = functions.Set_Values(
+                        self.parent.cam, exposure, gain, 114, False)
+                    # copy image from memory to numpy array
+                    functions.CopyImg(
+                        self.parent.cam,
+                        ImageData,
+                        self.parent.pcImgMem,
+                        self.parent.pid)
+                    self.img = ImageData.copy()
+                    self.img = np.roll(self.img, 15, axis=0)
+
+                    # calculate fft
+                    dft = functions.dft(self.img)
+                    fft = functions.fft_cv2(dft)
+                    fft = functions.fft_shift_py(
+                        fft.astype(np.float64)).astype(np.uint8)
+
+                    # display image
+                    res = np.concatenate((self.img, fft), axis=0)
+                    convertToQtFormat = qimage2ndarray.gray2qimage(res)
+                    p = convertToQtFormat.scaled(
+                        self.parent.bild_width,
+                        self.parent.bild_height,
+                        QtCore.Qt.KeepAspectRatio)
+                    self.changePixmap.emit(p)
+
+                    # save imgae in directory
+                    print("step\t" + str(self.j + 1) + "/" + str(self.max_int)
+                          + "\t " + str(self.parent.i) + "/" +
+                          str(self.parent.number_of_measurements))
+                    if mode is not None:
+                        tmp1 = "img" + mode
+                        tmp2 = "_%d_" % self.j + self.Max_Width.text()
+                        tmp3 = '_' + self.Temperature.text() + "_" + self.Current.text()
+                        tmp4 = "_" + self.number.text() + ".png"
+                        tmp5 = tmp1 + tmp2 + tmp3 + tmp4
+                        cv2.imwrite(os.path.join(
+                            self.string_direc2, tmp5), self.img)
+                    else:
+                        tmp1 = "img"
+                        tmp2 = "_%d_" % self.j + self.Max_Width.text()
+                        tmp3 = '_' + self.Temperature.text() + "_" + self.Current.text()
+                        tmp4 = "_" + self.number.text() + ".png"
+                        tmp5 = tmp1 + tmp2 + tmp3 + tmp4
+                        cv2.imwrite(os.path.join(
+                            self.string_direc2, tmp5), self.img)
+
+                    # end if max width is reached
+                    if self.i < float(self.Max_Width.text()):
+                        for k in range(int(self.number.text())):
+                            if self.parent.failed is True:
+                                break
+                            if self.mode == "forward":
+                                tmp = self.parent.position.copy()
+                                tmp[0][0] += 0.11
+                                self.parent.position = tmp
+                            elif self.mode == "backward":
+                                tmp = self.parent.position.copy()
+                                tmp[0][0] -= 0.11
+                                self.parent.position = tmp
+                            params = (self.parent.ser,)
+                            getattr(functions, self.mode)(*params)
+                            functions.save_txt(
+                                self.parent.pos_path, self.parent.position)
+                            self.msleep(10)
+                        self.do.emit()
+                        self.i += self.step
+                        self.j += 1
+                        self.msleep(150)
+                    else:
+                        self.ends = True
+                if self.i >= float(self.Max_Width.text()):
+                    break
+                if self.parent.ends is True:
+                    break
+                if self.parent.failed is True:
+                    break
+        print("Finished")
