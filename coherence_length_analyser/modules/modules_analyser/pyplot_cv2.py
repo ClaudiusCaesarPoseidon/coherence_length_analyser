b9adbc3cc0ffb000528b9591209a9f6aebb781a9
claudiuscaesarposeidon <haarmeyer@out-ev.de>
2019-03-06 15:53:28 +0100

    glubber

--- coherence_length_analyser/modules/modules_analyser/pyplot_cv2.py
+++ coherence_length_analyser/modules/modules_analyser/pyplot_cv2.py
@@ -364,7 +364,9 @@ class pyplot_cv2(QtCore.QThread):
                     elif tmp_for_filter == 1:
                         filter_string = "Moving Average\n"
                         filter_name = "m-a"
-                    #####
+                    elif tmp_for_filter == 2:
+                        filter_string = "SavitzkyGolay\n"
+                        filter_name = "sg"
                     #####
                     elif tmp_for_filter == 4:
                         filter_string = "Median\n"
