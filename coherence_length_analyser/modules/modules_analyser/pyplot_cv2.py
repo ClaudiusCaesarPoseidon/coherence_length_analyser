8fd66352985c13bdfd4db58e7b6951c5b92e871b
claudiuscaesarposeidon <haarmeyer@out-ev.de>
2019-03-06 15:59:15 +0100

    glubber

--- coherence_length_analyser/modules/modules_analyser/pyplot_cv2.py
+++ coherence_length_analyser/modules/modules_analyser/pyplot_cv2.py
@@ -368,19 +368,19 @@ class pyplot_cv2(QtCore.QThread):
                         filter_string = "Savitzky-Golay\n"
                         filter_name = "s-g"
                     elif tmp_for_filter == 3:
-                        filter_string = "first moving Average\nthen Savitzky–Golay"
+                        filter_string = "first moving Average\nthen Savitzky-Golay"
                         filter_name = "m-a_s-g"
                     elif tmp_for_filter == 4:
                         filter_string = "Median\n"
                         filter_name = "me"
                     elif tmp_for_filter == 5:
-                        filter_string = "First Median\nthen Savitzky–Golay"
+                        filter_string = "First Median\nthen Savitzky-Golay"
                         filter_name = "me_s-g"
                     elif tmp_for_filter == 6:
                         filter_string = "FFT\n"
                         filter_name = "fft"
                     elif tmp_for_filter == 7:
-                        filter_string = "First FFT\nthen Savitzky–Golay"
+                        filter_string = "First FFT\nthen Savitzky-Golay"
                         filter_name = "fft_s-g"
 ##################################################################################
                     # save plot as png
